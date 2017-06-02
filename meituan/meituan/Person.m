f89f65f9f7c317dd113fb998503a8eef1787b6e1
蒋琪 <asdasd@k.com>
2017-06-02 17:23:36 +0800

    add class of person

---
+++ meituan/meituan/Person.m
@@ -0,0 +1,21 @@
+//
+//  Person.m
+//  meituan
+//
+//  Created by 蒋琪 on 2017/6/2.
+//  Copyright © 2017年 jq. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
